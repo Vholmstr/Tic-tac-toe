d408f9a9eeff3b01d40c08e718c8e1290ddc2a66
Victor <holmstrom.victor@gmail.com>
2023-02-09 21:40:37 +0200

    change gameboard to contain values -1 and 1 instead of x and o

--- app.rb
+++ app.rb
@@ -2,15 +2,18 @@
 #puts "|x|o|o|"
 #puts "|o|x|o|"
 
+# x = 1     o = -1
+
 class GameBoard
   attr_reader :grid
 
   def initialize()
-    @grid = Array.new(3) {Array.new(3, " ")}
+    @grid = Array.new(3) {Array.new(3, 0)}
+    @winner = nil
   end
 
   def update_square(row, column, value)
-    if (@grid[row][column] == " ")
+    if (@grid[row][column] == 0)
       @grid[row][column] = value
       return true
     else
@@ -20,11 +23,21 @@ class GameBoard
 
   def print_board
     @grid.each do |row|
-      r = row.join("|")
-      puts "|#{r}|"
+      row_string = "|"
+      row.each do |value| 
+        if value == 1
+          row_string = row_string + "x|"
+        elsif value == -1
+          row_string = row_string + "o|"
+        else
+          row_string = row_string + " |"
+        end
+      end
+      puts row_string
     end
   end
 end
 
 game = GameBoard.new
+
 
