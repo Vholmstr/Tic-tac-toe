8d1dc5fda01626ceb1a203463a5d450acba7508c
Victor <holmstrom.victor@gmail.com>
2023-02-09 22:08:01 +0200

    add logic to check for a winner

--- app.rb
+++ app.rb
@@ -36,8 +36,58 @@ class GameBoard
       puts row_string
     end
   end
+
+  def check_for_winner
+    # Check rows
+    @grid.each do |row|
+      row_check = row.reduce(0) {|sum, val| sum += val}
+      if row_check == 3
+        @winner = "X"
+      elsif row_check == -3
+        @winner = "O"
+      else
+      end
+    end
+
+    # Check columns
+    for i in (0..2)
+      column_check = @grid.reduce(0) {|sum, row| sum += row[i]}
+      if column_check == 3
+        @winner = "X"
+      elsif column_check == -3
+        @winner = "O"
+      else
+      end
+    end
+    
+    # Check diagonals
+    diagonal_one = @grid[0][0] + @grid[1][1] + @grid[2][2]
+    diagonal_two = @grid[0][2] + @grid[1][1] + @grid[2][0]
+    if diagonal_one == 3 || diagonal_two == 3
+      @winner = "X"
+    elsif diagonal_one == -3 || diagonal_two == -3
+      @winner = "O"
+    else
+    end
+
+    # Check for a winner
+    if @winner != nil
+      puts "Winner: " + @winner
+      return true
+    else
+      return false
+    end
+  end
 end
 
 game = GameBoard.new
+game.print_board
+puts game.update_square(0,2,-1)
+game.print_board
+puts game.update_square(1,1,-1)
+game.print_board
+puts game.update_square(2,0,-1)
+game.print_board
+game.check_for_winner
 
 
